ebaf69a4c0648ed170e1414802a7033b2c1dbf9a
monglv <monglv@bkav.com>
2022-06-11 09:25:07 +0700

    MongLV: setup

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import "./styles.css";
+
+// sentry
+import * as Sentry from "@sentry/react";
+
+export default function App() {
+    const [message, setMessage] = useState("This is my app");
+    return (
+        <React.Fragment>
+            <Sentry.ErrorBoundary
+                fallback={({ error, componentStack, resetError }) => (
+                    <div className="App">
+                        <div>
+                            <div>You have encountered an error</div>
+                            <div style={{color: 'red'}}>{error.toString()}</div>
+                            <div style={{color: '#d7b700'}}>{componentStack}</div>
+                        </div>
+                    </div>
+                )}
+            >
+                <div className="App">
+                    <h1>Hello CodeSandbox</h1>
+                    <h2>Start editing to see some magic happen!</h2>
+                    <div>{message}</div>
+                    {/* on click, this button sets an Object as a message, not a string. */}
+                    {/* which will cause an error to occur in the component tree */}
+                    <button
+                        onClick={() => setMessage({super_bug: "Hello World"})}
+                    >
+                        Click here to change message!
+                    </button>
+                </div>
+            </Sentry.ErrorBoundary>
+        </React.Fragment>
+    );
+}
